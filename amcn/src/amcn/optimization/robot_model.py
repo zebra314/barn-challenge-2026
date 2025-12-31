a09aeb4b11da5d01e3629ef0547d8a298807a7ce
zebra314 <kidalfonso@gmail.com>
2026-01-01 03:56:16 +0800

    Update unicycle model to skid steer model

--- amcn/src/amcn/optimization/robot_model.py
+++ amcn/src/amcn/optimization/robot_model.py
@@ -1,37 +1,50 @@
 import casadi as ca
-import numpy as np
 
 class JackalModel:
     def __init__(self, dt):
         self.dt = dt
+        self.turning_efficiency = 1.0
+
+        # Instantaneous center of rotation distance from robot center
+        self.x_icr = 0.0
 
     def get_kinematics(self):
         """
-
-        State: [x, y, theta]
-        Control: [v, omega]
+        Returns the Kinematic Skid-Steer Model
         """
-        #
+
+        # State
         x = ca.SX.sym('x')
         y = ca.SX.sym('y')
         theta = ca.SX.sym('theta')
         states = ca.vertcat(x, y, theta)
 
-        v = ca.SX.sym('v')
-        omega = ca.SX.sym('omega')
-        controls = ca.vertcat(v, omega)
+        # Control
+        v_cmd = ca.SX.sym('v_cmd')
+        omega_cmd = ca.SX.sym('w_cmd')
+        controls = ca.vertcat(v_cmd, omega_cmd)
 
-        # Differential Drive Kinematics
-        # x_dot = v * cos(theta)
-        # y_dot = v * sin(theta)
-        # theta_dot = omega
-        rhs = ca.vertcat(v * ca.cos(theta),
-                         v * ca.sin(theta),
-                         omega)
+        # Convert commands to body frame velocities
+        v_x_body = v_cmd
+        omega_body = omega_cmd * self.turning_efficiency
 
-        k1 = rhs
-        k2_state = states + 0.5 * self.dt * k1
-        states_next = states + rhs * self.dt
+        # Calculate lateral velocity based on ICR
+        v_y_body = self.x_icr * omega_body
 
-        f = ca.Function('F', [states, controls], [states_next])
-        return f
+        # Calculate world frame velocities
+        x_dot = v_x_body * ca.cos(theta) - v_y_body * ca.sin(theta)
+        y_dot = v_x_body * ca.sin(theta) + v_y_body * ca.cos(theta)
+        theta_dot = omega_body
+
+        rhs = ca.vertcat(x_dot, y_dot, theta_dot)
+
+        # Runge-Kutta 4 Integration
+        f_dynamic = ca.Function('f_dynamic', [states, controls], [rhs])
+
+        k1 = f_dynamic(states, controls)
+        k2 = f_dynamic(states + self.dt / 2 * k1, controls)
+        k3 = f_dynamic(states + self.dt / 2 * k2, controls)
+        k4 = f_dynamic(states + self.dt * k3, controls)
+        states_next = states + (self.dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+        return ca.Function('F', [states, controls], [states_next])
